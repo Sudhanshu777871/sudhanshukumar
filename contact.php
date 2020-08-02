4bcf889af9947ff3467f5c2c83c69c84b209d43a
Sudhanshu777871 <59053168+Sudhanshu777871@users.noreply.github.com>
2020-08-02 22:46:00 +0530

    sudhanshukumar

---
+++ contact.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="TemplateMo">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Contact Us</title>
+    <link rel="icon" type="image/png" href="assets/images/logo.PNG">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/templatemo-finance-business.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+<!--
+
+Finance Business TemplateMo
+
+https://templatemo.com/tm-545-finance-business
+
+-->
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="mailto: ksudhanshu394@gmail.com"><i style="font-size:24px"
+                class="fa">&#xf0e0;</i></i>ksudhanshu394@gmail.com</a></li>
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    
+    <header class="">
+      <nav class="navbar navbar-expand-lg">
+        <div class="container">
+          <a class="navbar-brand" href="/portfolio/index.php"><h2> <img src="assets/images/logo.PNG" width="40" height="40" style="margin-right: 15px;border-radius: 30px;">Sudhanshu Kumar</h2>
+          </h2></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="/portfolio/index.php">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+             
+              <li class="nav-item active">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+
+    <!-- Page Content -->
+    <div class="page-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1>Contact Us</h1>
+            <span>feel free to contact us!</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="contact-information">
+      <div class="container"  style="margin-bottom: 80px;">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="contact-item">
+              <i class="fa fa-phone"></i>
+              <h4>Phone</h4>
+              <a href="tel:9839191440">9839191440</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="contact-item">
+              <i class="fa fa-envelope"></i>
+              <h4>Email</h4>
+        
+            <a href="mailto: ksudhanshu394@gmail.com">ksudhanshu394@gmail.com</a>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="contact-item">
+              <i class="fa fa-map-marker"></i>
+              <h4>Location</h4>
+              <p> Gomti Nagar Lucknow, 226011</p>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+
+    <div id="map">
+<!-- How to change your own map point
+	1. Go to Google Maps
+	2. Click on your location point
+	3. Click "Share" and choose "Embed map" tab
+	4. Copy only URL and paste it within the src="" field below
+-->
+      <iframe src="https://maps.google.com/maps?q=Vibhuti+Khand+Gomti+Nagar+Lucknow+India&t=&z=13&ie=UTF8&iwloc=&output=embed" width="100%" height="500px" frameborder="0" style="border:0" allowfullscreen></iframe>
+    </div>
+
+
+
+
+    <!-- Footer Starts Here -->
+    
+    <div class="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <p>Copyright &copy; 2020 Sudhanshu Kumar
+  
+              - Web Design & Developer</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="The Portfolio Of Sudhanshu Kumar- Web Designer And developer">
+  <meta name="author" content="Sudhanshu Kumar">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
+    rel="stylesheet">
+<!-- boostrap coding start -->
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+  
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+<!-- boostrap coding ended -->
+  <title> Sudhanshu Kumar | Web Designer & Developer</title>
+  <link rel="icon" type="image/png" href="assets/images/logo.PNG">
+
+
+
+   
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/templatemo-finance-business.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+  <!--
+
+Finance Business TemplateMo
+
+https://templatemo.com/tm-545-finance-business
+
+-->
+
+
+
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="preloader">
+    <div class="jumper">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-xs-12">
+          <ul class="left-info">
+
+            <li><a href="mailto: ksudhanshu394@gmail.com"><i style="font-size:24px"
+                  class="fa">&#xf0e0;</i></i>ksudhanshu394@gmail.com</a></li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <header class="">
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="index.php">
+          <h2> <img src="assets/images/logo.PNG" width="40" height="40" style="margin-right: 15px;border-radius: 30px;">Sudhanshu Kumar</h2>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+          aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+          
+            <li class="nav-item">
+              <a class="nav-link" href="/portfolio/contact.php">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+
+  <!-- Page Content -->
+  <!-- Banner Starts Here -->
+  <div class="main-banner header-text" id="top">
+    <div class="Modern-Slider">
+      <!-- Item -->
+      <div class="item item-1">
+        <div class="img-fill">
+          <div class="text-content">
+            <h6>A Very Welcome To You</h6>
+            <h4>I' Am Full Stack Web Developer<br>&amp; Sudhanshu Kumar</h4>
+            <p>I am Sudhanshu Kumar, Full Stack Web Developer. I am Working In This Field For More Than 3+ Years As A
+              Professional Full Stack Developer. And My Main Motto Is Client Satisfaction, That's Why I Serve My
+              Services With More Than 300+ Happy Clients. </p>
+            <a href="/portfolio/contact.php" class="filled-button">Contact Me</a>
+          </div>
+        </div>
+      </div>
+      <!-- // Item -->
+      <!-- Item -->
+
+      <!-- // Item -->
+      <!-- Item -->
+
+      <!-- // Item -->
+    </div>
+  </div>
+  <!-- Banner Ends Here -->
+
+  <div class="request-form">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <h4>Want Message Me, Contact Us. </h4>
+          <span>I Always Available For Our Clients.....</span>
+        </div>
+        <div class="col-md-4">
+          <a href="/portfolio/contact.php" class="border-button">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>Our <em>Services</em></h2>
+            <span>What Services you will Get from me!</span>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-item" style="border: 4px solid black; border-radius: 5px;">
+            <img src="assets/images/service_01.jpg" alt="" width="100" height="170"
+              style="border-bottom:2px solid black">
+            <div class="down-content">
+              <h4>UI/UX DESIGN</h4>
+
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-item" style="border: 4px solid black; border-radius: 5px;">
+            <img src="assets/images/service_02.jpg" alt="" width="100" height="170"
+              style="border-bottom:2px solid black">
+            <div class="down-content">
+              <h4>Website Design</h4>
+
+
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="service-item" style="border: 4px solid black; border-radius: 5px;">
+            <img src="assets/images/service_03.jpg" alt="" width="100" height="170"
+              style="border-bottom:2px solid black">
+            <div class="down-content">
+              <h4>Website Development</h4>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="fun-facts">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="left-content">
+            <span>Sudhanshu Kumar</span>
+            <h2>Our Progress Are Reguarly Increases...... <br><em>Our Growth</em></h2>
+
+          </div>
+        </div>
+        <div class="col-md-6 align-self-center">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="count-area-content">
+                <div class="count-digit">300</div>
+                <div class="count-title">Happy Clients</div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="count-area-content">
+                <div class="count-digit">8</div>
+                <div class="count-title">Members In Our Team</div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="count-area-content">
+                <div class="count-digit">400</div>
+                <div class="count-title">Projects Done On Time</div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="count-area-content">
+                <div class="count-digit">3</div>
+                <div class="count-title">Times Repeatable Clients</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+<!-- coding of previous work link -->
+<div class="container" style="margin-top: 40px;background-color: rgb(231, 224, 238);padding: 40px;">
+<h1 style="text-align: center;color: black;font-weight: bolder;">Our <b style="color: rgb(103, 224, 103);">Previous Projects</b></h1>
+
+<p style="text-align: center;color: rgb(49, 46, 46);font-weight: bolder;margin-top: 20px;margin-bottom: 50px;">Here Is A List of Our Previous Website Making Projects</p>
+
+<ul style="list-style-type:square;">
+  <li><a href="https://www.sandiegobrewing.com" target="_blank">sandiegobrewing.com</a></li>
+  <li><a href="https://www.culinariafoodandwine.com" target="_blank">culinariafoodandwine.com</a></li>
+<li><a href="https://generatepress.com" target="_blank">generatepress.com</a></li>
+<li><a href="http://impartacomputersolutions.com/" target="_blank">impartacomputersolutions.com</a></li>
+
+<li><a href="http://www.srmcem.ac.in/" target="_blank">srmcem.ac.in</a></li>
+<li><a href="https://www.olivegarden.com" target="_blank">www.olivegarden.com</a></li>
+
+<li><a href="https://www.swell.com" target="_blank">swell.com</a></li>
+<li><a href="https://www.etq-amsterdam.com" target="_blank">etq-amsterdam.com</a></li>
+<li><a href="http://yeahburger.com/" target="_blank">yeahburger.com</a></li>
+
+</ul>
+</div>
+  <div class="more-info">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="more-info-content">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="left-image">
+                  <img src="assets/images/more-info.jpg" alt="" style="width:450px;height:400px;">
+                </div>
+              </div>
+              <div class="col-md-6 align-self-center">
+                <div class="right-content">
+                  <span>Why Choose Us -</span>
+                  <h2>Why Choose Us <em>Sudhanshu Kumar</em></h2>
+                  <ul style="list-style-type: square">
+                    <li>For Quick And Secure Websites.</li>
+                    <li>For SEO Friendly Websites</li>
+                    <li>For Responsive Websites</li>
+                    <li>For Static And Dynamic Websites At Your Own Budget.</li>
+                    <li>For Complete Website Setup Form Designing To Hosting.</li>
+
+                  </ul>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-heading">
+            <h2>What Clients Say <em>About Us</em></h2>
+            <span>Recently Feedback Of Our Clients</span>
+          </div>
+        </div>
+        <div class="col-md-12">
+          <div class="owl-testimonials owl-carousel">
+
+            <div class="testimonial-item">
+              <div class="inner-content">
+                <h4>Akhilesh Sahu</h4>
+                <span>Resturant Owner</span>
+                <p>"Such A Great Service Of Sudhanshu Kumar, I am Really Impress For His Quick And Secure Website
+                  Making."</p>
+              </div>
+
+            </div>
+
+            <div class="testimonial-item">
+              <div class="inner-content">
+                <h4>Vijay Sonar</h4>
+                <span>Market Specialist</span>
+                <p>"Thanks Sudhanshu Kumar For Your Making This Awesome And Secure Website At My Own Budget."</p>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+      </div>
+    </div>
+  </div>
+
+      
+  <div class="sub-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <p>Copyright &copy; 2020 Sudhanshu Kumar
+
+            - Web Design & Developer</a></p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Additional Scripts -->
+  <script src="assets/js/custom.js"></script>
+  <script src="assets/js/owl.js"></script>
+  <script src="assets/js/slick.js"></script>
+  <script src="assets/js/accordions.js"></script>
+
+  <script language="text/Javascript">
+    cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+    function clearField(t) {                   //declaring the array outside of the
+      if (!cleared[t.id]) {                      // function makes it static and global
+        cleared[t.id] = 1;  // you could use true and false, but that's more typing
+        t.value = '';         // with more chance of typos
+        t.style.color = '#fff';
+      }
+    }
+  </script>
+
+</body>
+
+</html>
